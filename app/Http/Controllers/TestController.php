b7076b14cb66af87ca0f04e43b6f166645f2448e
Vladimir Valakh <sinclair.ubuntu@gmail.com>
2023-11-16 23:58:10 +0300

    send message to telegram

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,15 +8,16 @@ class TestController extends Controller
 {
     public function sendMail(NotificationService $notificationService)
     {
-        $to_email = "sinclair.ubuntu@gmail.com";
-        $to_name = "Sinclair";
-        $from_email = "sinclair.ubuntu@gmail.com";
-        $from_name = "ЛидМаркетРФ";
-        $subject = "Новая заявка";
-        $email_message = "Вы получили новую заявку";
-
-
-        $notificationService->sendEmail($to_email, $to_name, $from_email, $from_name, $subject, $email_message);
+//        $to_email = "sinclair.ubuntu@gmail.com";
+//        $to_name = "Sinclair";
+//        $from_email = "sinclair.ubuntu@gmail.com";
+//        $from_name = "ЛидМаркетРФ";
+//        $subject = "Новая заявка";
+//        $email_message = "Вы получили новую заявку";
+//
+//
+//        $notificationService->sendEmail($to_email, $to_name, $from_email, $from_name, $subject, $email_message);
+        $notificationService->sendToTelegram(86, "Hello!!!\nКак сам?");
         var_dump('ok');
         die();
     }
--- app/Services/NotificationService.php
+++ app/Services/NotificationService.php
@@ -5,10 +5,19 @@ declare(strict_types=1);
 namespace App\Services;
 
 use Illuminate\Support\Facades\Mail;
+use App\Repositories\UserRepository;
 
 
 class NotificationService
 {
+    private UserRepository $userRepository;
+
+    public function __construct(
+        UserRepository $userRepository,
+    ) {
+        $this->userRepository = $userRepository;
+    }
+
     public function sendEmail($to_email, $to_name, $from_email, $from_name, $subject, $email_message)
     {
         $data = [
@@ -28,5 +37,19 @@ class NotificationService
             $message->from($from_email, $from_name);
         });
 
+    }
+
+    public function sendToTelegram($user_id, $message)
+    {
+        $telegramId = $this->userRepository->getUser($user_id)->telegram_id;
+        $telegramBotToken = env('TELEGRAM_BOT_TOKEN');
+
+        $message = urlencode($message);
+
+        try {
+            file_get_contents("https://api.telegram.org/bot$telegramBotToken/sendMessage?chat_id=$telegramId&text=" . $message);
+        } catch (\Exception $e){
+            var_dump($e->getMessage());
+        }
     }
 }
